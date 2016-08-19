1c9b184a0605b3774d977e1e30be4f0440343706
David <dabitp@gmail.com>
2016-08-19 08:06:33 -0700

    Making all channels lowercase
    Not all clients send the correct channel name and some servers will pass
    right on the channel name from the client. Adding an optional "lower
    case" conversion for the channel name. Defaults to true.

--- src/Channel.ts
+++ src/Channel.ts
@@ -6,11 +6,11 @@ export class Channel implements Target.ITarget {
     display: string;
     topic: Topic;
 
-    constructor(display:string) {
+    constructor(display:string, tolower:boolean = true) {
         // ':' will NEVER be allowed as a channel name (reserved character). remove it if it's in here
         if (display[0] == ':') display = display.substr(1);
-
         this.display = display;
+        if (tolower) this.display = this.display.toLocaleLowerCase();
     }
 
     toString() :string {
